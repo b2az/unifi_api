67c9a822c3ae9c5948cd23dce460b36d4ae73686
b2az <b2az@protonmail.com>
2020-12-04 10:24:45 +0100

    up

--- custom_components/unifi_api/__init__.py
+++ custom_components/unifi_api/__init__.py
@@ -1,7 +1,10 @@
 from homeassistant import core
+import voluptuous as vol
 
+from homeassistant.helpers import config_validation as cv
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Unifi API component."""
+    hass.states.set("hello_world.Hello_World", "Works!")
     # @TODO: Add setup code.
     return True
